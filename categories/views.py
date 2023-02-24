c828b0285c894ca45164f75375d5b81d4654808b
moonk <mkjoehsb@gmail.com>
2023-02-25 08:29:11 +0900

    serializer 사용법 10.5

--- categories/views.py
+++ categories/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
-        {
-            "ok": True,
-            "categories": serializers.serialize("json", all_categories),
-        }
-    )
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(
+            all_categories,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = CategorySerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            return Response(
+                {"created": True},
+            )
+        else:
+            return Response(serializer.errors)
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
