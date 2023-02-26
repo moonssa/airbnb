6f5ba7c9a28c26284d3a97bd3956f257d37619b4
moonk <mkjoehsb@gmail.com>
2023-02-26 09:18:13 +0900

    automation serializers

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,32 +2,9 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-
-    # 카테고리 필드중에 어떤 부분을 보여줄지 명시한다.
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(
-        read_only=True
-    )
-
-    def create(self, validated_data):
-        return Category.objects.create(
-            **validated_data
-        )
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get(
-            "name", instance.name
-        )
-        instance.kind = validated_data.get(
-            "kind", instance.kind
-        )
-        instance.save()
-        return instance
+class CategorySerializer(
+    serializers.ModelSerializer
+):
+    class Meta:
+        model = Category
+        fields = "__all__"
